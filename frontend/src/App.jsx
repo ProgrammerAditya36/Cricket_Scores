80f9e7edf335cb24f8871111b6911d02965c2be1
Aditya Amit Kinjawadekar <programmeraditya36@gmail.com>
2024-07-11 03:32:04 +0530

    Frontend Style changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,14 @@
 import "./App.css";
-import Card from "./components/Card";
 import "./index.css";
 import React from "react";
-import useData from "./hooks/useData";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Scores from "./Scores";
-import Upload from "./Upload";
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Scores />} />
-                <Route path="/upload" element={<Upload />} />
             </Routes>
         </BrowserRouter>
     );
--- frontend/src/Scores.jsx
+++ frontend/src/Scores.jsx
@@ -1,43 +1,64 @@
 import "./App.css";
 import Card from "./components/Card";
 import "./index.css";
-import React from "react";
-import useData from "./hooks/useData";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-function Scores() {
-    const data = useData("https://cricket-scores-api.vercel.app/");
-    const navigate = useNavigate();
-    if (!data.loading) {
-        const scores = data.data;
+import axios from "axios";
 
-        return (
-            <>
-                <div className="bg-slate-200 h-screen">
-                    <h1 className="text-4xl text-center font-bold p-4">
-                        Cricket Scores
-                    </h1>
-                    <div className="flex flex-wrap justify-center align-middle p-4 ">
-                        {scores.map((score) => {
-                            return (
-                                <Card
-                                    key={score._id}
-                                    name={score.name}
-                                    runs={score.runs}
-                                    wickets={score.wickets}
-                                    updateFn={() => {
-                                        navigate(
-                                            `/upload?id=${score._id}&name=${score.name}&runs=${score.runs}&wickets=${score.wickets}`
-                                        );
-                                    }}
-                                ></Card>
-                            );
-                        })}
-                    </div>
-                </div>
-            </>
-        );
+function Scores() {
+    const [scores, setScores] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get("https://cricket-scores-api.vercel.app/").then((res) => {
+            const data = res.data;
+            setScores(data);
+            setLoading(false);
+        });
+    }, []);
+
+    const handleUpdate = (updatedScore) => {
+        setScores((prevScores) => {
+            return prevScores.map((score) =>
+                score._id === updatedScore._id ? updatedScore : score
+            );
+        });
+
+        // Send the updated score to the server
+        axios
+            .put(`https://cricket-scores-api.vercel.app/${updatedScore._id}`, updatedScore)
+            .then((response) => {})
+            .catch((error) => {
+                console.error("There was an error updating the score:", error);
+            });
+    };
+
+    if (loading) {
+        return <h1>Loading...</h1>;
     }
-    return <h1>Loading...</h1>;
+
+    return (
+        <>
+            <div className="bg-slate-700 md:h-screen px-10">
+                <h1 className="text-4xl text-white text-center font-bold pt-20 mb-20">
+                    Cricket Scores
+                </h1>
+                <div className="flex flex-col md:flex-row justify-evenly items-center overflow-visible">
+                    {scores.map((score) => {
+                        return (
+                            <Card
+                                key={score._id}
+                                score={score}
+                                handleUpdate={handleUpdate}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Scores;
