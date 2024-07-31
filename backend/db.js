41bde9b7d0c3b7e107ddb337611212bfeea4de23
Aditya Amit Kinjawadekar <programmeraditya36@gmail.com>
2024-07-31 19:50:49 +0530

    Auto commit

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:232004Aditya%40mongo@cluster0.dahrvb8.mongodb.net/cricket_scores');
+mongoose.connect('mongodb+srv://admin:test123@cluster0.dahrvb8.mongodb.net/cricket_scores');
 const UserSchema = new mongoose.Schema({
     name: String,
     runs: Number,
